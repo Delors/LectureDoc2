59efc4e7e3ca9e815a241e1025623a2e88e27772
Michael Eichberg <mail@michael-eichberg.de>
2025-05-15 11:06:25 +0200

    improving documentation

--- renaissance/js/ld-tables.js
+++ renaissance/js/ld-tables.js
@@ -37,10 +37,8 @@ function afterLDDOMManipulations() {
 const tables = [];
 
 /**
- * This function is called after all listener registrations related
- * to the core functionality of LectureDoc have been done.
- *
- * Use this method to register additional listeners.
+ * We are using this callback function, which is called after all listener registrations related
+ * to the core functionality of LectureDoc have been done, to register a hover listener.
  */
 function afterLDListenerRegistrations() {
     console.log("performing ld-tables.afterLDListenerRegistrations");
@@ -329,131 +327,6 @@ function afterLDListenerRegistrations() {
         }
     });
 
-    /*
-        document
-            .querySelectorAll("#ld-slides-pane table.highlight-cell-on-hover")
-            .forEach((table) => {
-                const tbody = table.querySelector(":scope tbody");
-                // th.stub is used by rst to identify cells in columns that
-                // serve as row headers
-                tbody.querySelectorAll(":scope td").forEach((td) => {
-                    td.addEventListener("mouseenter", () => addHoverState(td));
-                    td.addEventListener("mouseleave", () => removeHoverState(td));
-                });
-            });
-
-        document
-            .querySelectorAll("#ld-slides-pane table.highlight-row-on-hover")
-            .forEach((table) => {
-                table.querySelectorAll(":scope tbody tr").forEach((tr) => {
-                    tr.addEventListener("mouseenter", () => addHoverState(tr));
-                    tr.addEventListener("mouseleave", () => removeHoverState(tr));
-                });
-            });
-         */
-
-    /**
-         * The following highlights the current element and the element in
-         * the first row with the same column and in the first column with the
-         * same row.
-
-        document
-            .querySelectorAll("#ld-slides-pane table.highlight-on-hover")
-            .forEach((table) => {
-                function highlight(cell, hovered) {
-                    const cellIndex = cell.cellIndex; // <=> columnIndex
-                    const rowIndex = cell.parentElement.rowIndex;
-                    const hoverRelatedCells = [];
-                    //table.rows[rowIndex].cells[cellIndex].classList.add(":hover");
-                    if (rowIndex !== 0)
-                        hoverRelatedCells.push(table.rows[0].cells[cellIndex]);
-                    if (cellIndex !== 0)
-                        hoverRelatedCells.push(table.rows[rowIndex].cells[0]);
-                    if (hovered) addHoverState(cell, ...hoverRelatedCells);
-                    else removeHoverState(cell, ...hoverRelatedCells);
-                }
-
-                const tbody = table.querySelector(":scope tbody");
-                // th.stub is used by rst to mark up data cells in columns that
-                // serve as row headers
-                tbody.querySelectorAll(":scope td").forEach((td) => {
-                    td.addEventListener("mouseenter", () => highlight(td, true));
-                    td.addEventListener("mouseleave", () => highlight(td, false));
-                });
-            });
-            */
-    /*
-    document
-        .querySelectorAll(
-            "#ld-slides-pane table.highlight-identical-cells-on-hover",
-        )
-        .forEach((table) => {
-            const tbody = table.querySelector(":scope tbody");
-            function eq(nl1, nl2) {
-                return (
-                    nl1.length === nl2.length &&
-                    Array.from(nl1).every((v, i) => v.isEqualNode(nl2[i]))
-                );
-            }
-            function highlightValue(baseTD) {
-                console.log(
-                    baseTD.cellIndex +
-                        " " +
-                        baseTD.parentNode.rowIndex +
-                        " " +
-                        baseTD.parentNode.sectionRowIndex,
-                );
-                baseTD.classList.add(":hover");
-                tbody.querySelectorAll(":scope td").forEach((td) => {
-                    //if (eq(baseTD.childNodes, td.childNodes)) {
-                    if (baseTD.textContent === td.textContent) {
-                        td.classList.add(":hover-related");
-                    }
-                });
-            }
-            function dehighlightValue(baseTD) {
-                tbody.querySelectorAll(":scope td").forEach((td) => {
-                    // if (eq(baseTD.childNodes, td.childNodes)) {
-                    if (baseTD.textContent === td.textContent) {
-                        td.classList.remove(":hover-related");
-                    }
-                });
-                baseTD.classList.remove(":hover");
-            }
-
-            tbody.querySelectorAll(":scope td").forEach((td) => {
-                td.addEventListener("mouseenter", () => highlightValue(td));
-                td.addEventListener("mouseleave", () => dehighlightValue(td));
-            });
-        });
-*/
-    /*
-        document
-            .querySelectorAll(
-                "#ld-slides-pane table.highlight-cell-and-row-on-hover",
-            )
-            .forEach((table) => {
-                function highlightRow(baseTD) {
-                    baseTD.classList.add(":hover");
-                    const tr = baseTD.parentNode;
-                    const tds = Array.from(tr.cells);
-                    tds.forEach((td) => {
-                        if (baseTD !== td) td.classList.add(":hover-related");
-                    });
-                }
-                function dehighlightRow(baseTD) {
-                    Array.from(baseTD.parentNode.cells).forEach((td) => {
-                        td.classList.remove(":hover-related");
-                    });
-                    baseTD.classList.remove(":hover");
-                }
-
-                table.querySelectorAll(":scope td").forEach((td) => {
-                    td.addEventListener("mouseenter", () => highlightRow(td));
-                    td.addEventListener("mouseleave", () => dehighlightRow(td));
-                });
-            });
-        */
 }
 
 const ldEvents = lectureDoc2.ldEvents;
